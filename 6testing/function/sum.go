f27267df5d5bb30ebeda4998b7ca28bbbcfbaa9a
Atha Dhaiffathin <atha.dhaiffathin@jatis.com>
2023-01-13 19:30:59 +0700

    add dokumentasi fungsi

--- 6testing/function/sum.go
+++ 6testing/function/sum.go
@@ -1,6 +1,14 @@
 package function
 
-// deklarasi fungsi yang ingin di test, huruf pertama kapital agar menjadi public
+/*
+	contoh dokumentasi fungsi, hover ke sum.
+*/
+/*
+	deklarasi fungsi yang ingin di test, huruf pertama kapital agar menjadi public
+*/
+/*
+	Sum(2, 3)
+*/
 func Sum(a, b int) int {
 	return a + b
 }
